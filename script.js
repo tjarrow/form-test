dcb16a76c9b8116fa90da94743b1ccc6ea2146ff
tjarrow <nickita0104@gmail.com>
2018-08-28 13:33:48 +0300

    Initital commit

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+	 pattern1 = '', pattern2 = '';
+
+			$('#email').blur(function() {
+				if($(this).val() != '') {
+					 pattern1 = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+					if(pattern1.test($(this).val())){
+						$(this).css({'border' : '1px solid #569b44'});
+            $('#valid-mail').remove();
+					} else {
+						if (!($('#email').next().attr('id')=='valid-mail')) {
+							$(this).css({'border' : '1px solid #ff0000'});
+	            $('#email').after("<span id = 'valid-mail'></span>");
+							$('#valid-mail').text('Неверно введён e-mail');
+						}
+					}
+				}
+			});
+
+      $('#phone').blur(function() {
+				if($(this).val() != '') {
+					 pattern2 = /^\d[\d\(\)\ -]{4,14}\d$/;
+					if(pattern2.test($(this).val())){
+						$(this).css({'border' : '1px solid #569b44'});
+            $('#valid-phone').remove();
+					} else {
+						if (!($('#phone').next().attr('id')=='valid')) {
+							$(this).css({'border' : '1px solid #ff0000'});
+	            $('#phone').after("<span id = 'valid-phone'></span>");
+							$('#valid-phone').text('Неверно введён номер телефона');
+						}
+					}
+				}
+			});
+
+			$("form").submit(function() {
+				if ((pattern1.test($("#email").val())) && (pattern2.test($("#phone").val()))) {
+						window.location.href = "https://google.com";
+					return true;
+				} else return false;
+
+			});
+
+		});
